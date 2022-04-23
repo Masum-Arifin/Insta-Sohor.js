698158e094318f1ae41c463dddb21bf814290ee8
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-24 00:03:14 +0600

    js

--- js/app.js
+++ js/app.js
@@ -28,11 +28,11 @@ const reportPost = (id) => {
   showPosts(remainingPosts);
 };
 
-// const displayContent = (text) => {
-//   return text.length < 30
-//     ? text
-//     : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-// };
+const displayContent = (text) => {
+  return text.length < 30
+    ? text
+    : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+};
 
 const switchTab = (id) => {
   if (id === "posts") {
