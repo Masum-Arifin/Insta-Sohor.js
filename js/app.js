eaaab1e00e5a0ffd3c42751c8995240c4e4fe062
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-29 00:05:36 +0600

    js

--- js/app.js
+++ js/app.js
@@ -41,8 +41,8 @@ const switchTab = (id) => {
     document.getElementById("reported").style.display = "none";
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
-    // document.getElementById("posts").style.display = "none";
-    document.getElementById("reported").style.display = "none";
+    document.getElementById("posts").style.display = "none";
+    // document.getElementById("reported").style.display = "none";
 
     displayLikedPosts();
   } else {
