83d97645ac7a55ac421738f72ba7ee12acf55a4a
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-30 00:09:20 +0600

    js

--- js/app.js
+++ js/app.js
@@ -60,12 +60,12 @@ const createPost = (post) => {
   div.classList.add("post");
   div.innerHTML = `
               <div class="post__header">
-                // <div class="post__profile">
-                  <a
-                    href="https://github.com/ProgrammingHero1"
-                    target="_blank"
-                    class="post__avatar"
-                  >
+                <div class="post__profile">
+                  // <a
+                  //   href="https://github.com/ProgrammingHero1"
+                  //   target="_blank"
+                  //   class="post__avatar"
+                  // >
                     <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
